4e261f155e69c62f1aaa0a8e3ba438b0c453c0d3
Du.Kang <banshee1115@163.com>
2024-06-13 20:54:06 +0800

    引入three.js

--- script.js
+++ script.js
@@ -6,4 +6,7 @@
  * @FilePath: /threejs-learning/script.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+import * as THREE from 'three'
 console.log('JavaScript is working')
+console.log("🚀 ~ THREE:", THREE)
+
