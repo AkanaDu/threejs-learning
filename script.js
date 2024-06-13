28d1bfdec7baf650c66fc2885760c7066f639d76
Du.Kang <banshee1115@163.com>
2024-06-13 23:14:27 +0800

    设置坐标轴，红色x，绿色y，蓝色z

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * @Author: Du.Kang banshee1115@163.com
  * @Date: 2024-06-13 20:47:38
  * @LastEditors: Du.Kang banshee1115@163.com
- * @LastEditTime: 2024-06-13 23:06:30
+ * @LastEditTime: 2024-06-13 23:12:26
  * @FilePath: /threejs-learning/script.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,6 +25,10 @@ const mesh = new THREE.Mesh(geometry, material) // 创建网格体
 mesh.position.set(0.7, -1.6, 1)
 scene.add(mesh) // 将网格体添加至场景中 
 
+// Axes helper
+const axes = new THREE.AxesHelper()
+scene.add(axes)
+
 // Size
 const sizes = { 
   width: 800,
@@ -33,7 +37,9 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // 视角 长宽比 
-camera.position.z = 3
+camera.position.z = 5
+camera.position.y = 1
+camera.position.x = 1
 scene.add(camera) // 场景中添加相机
 
 // Renderer
