c67d008111b4c0cf848732008dac23d6fa358d61
Du.Kang <banshee1115@163.com>
2024-06-13 20:51:00 +0800

    添加文件

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Du.Kang banshee1115@163.com
+ * @Date: 2024-06-13 20:47:38
+ * @LastEditors: Du.Kang banshee1115@163.com
+ * @LastEditTime: 2024-06-13 20:47:51
+ * @FilePath: /threejs-learning/script.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('JavaScript is working')
