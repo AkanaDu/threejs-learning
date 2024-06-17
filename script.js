28a67d085f342f76a460a5e90e44c529a773a3b8
Du.Kang <banshee1115@163.com>
2024-06-17 22:28:28 +0800

    正交相机

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * @Author: Du.Kang banshee1115@163.com
  * @Date: 2024-06-13 20:47:38
  * @LastEditors: Du.Kang banshee1115@163.com
- * @LastEditTime: 2024-06-14 13:55:41
+ * @LastEditTime: 2024-06-17 22:26:55
  * @FilePath: /threejs-learning/script.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,8 +28,21 @@ const sizes = {
 }
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // 视角 长宽比 
-camera.position.z = 3
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // 视角 长宽比 
+// const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100) // 画面是扁的
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1,
+  -1,
+  0.1,
+  100
+)
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
 scene.add(camera) // 场景中添加相机
 
 // Renderer
@@ -47,9 +60,9 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime()
   
   // Update object
-  mesh.rotation.x = elapsedTime
+  // mesh.rotation.x = elapsedTime
   mesh.rotation.y = elapsedTime
-  mesh.rotation.z = elapsedTime
+  // mesh.rotation.z = elapsedTime
 
   // Render
   renderer.render(scene, camera)
